1c87674d1837767a935a89b33b9bff27a6664929
Ilia <eilia.abedian@yahoo.com>
2025-02-15 17:36:25 -0800

    chore: healthcheck api every 2 seconds but show the message once

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -2,8 +2,9 @@
 
 docker compose up --build -d
 
+echo "Waiting for 'sonar-watch-portfolio-api' healthcheck result..."
+
 until docker inspect --format='{{json .State.Health.Status}}' sonar-watch-portfolio-api | grep -q "healthy"; do
-    echo "Waiting for 'sonar-watch-portfolio-api' healthcheck result..."
-    sleep 20
+    sleep 2
 done
 echo "Container is healthy!"
